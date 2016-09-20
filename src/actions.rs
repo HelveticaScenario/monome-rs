6ff85efb0aa0b74940f73c079c7b1892d4801edd
Michael Kedzierski <michael@kedzierski.id.au>
2016-09-20 21:46:26 +1000

    Started adding some tests.

--- src/actions.rs
+++ src/actions.rs
@@ -4,6 +4,7 @@ use super::errors::*;
 use super::constants::PREFIX;
 
 // TODO: split out grid and add tilt & arc support
+// TODO: support setting intensities in various actions
 #[allow(enum_variant_names)]
 pub enum MonomeAction<'a> {
     LedSet(u8, u8, bool),
@@ -41,7 +42,7 @@ impl<'a> MonomeAction<'a> {
             .into()
     }
 
-    pub fn to_args(&self) -> Vec<OscType> {
+    fn to_args(&self) -> Vec<OscType> {
         match *self {
             MonomeAction::LedSet(x, y, s) => {
                 vec![OscType::Int(x as i32), OscType::Int(y as i32), OscType::Int(s as i32)]
@@ -64,5 +65,80 @@ impl<'a> MonomeAction<'a> {
                 vec![OscType::Int(x_off as i32), OscType::Int(y as i32), OscType::Int(mask as i32)]
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rosc::{OscPacket, OscMessage, OscType};
+
+    #[test]
+    fn led_set_to_packet() {
+        let packet = MonomeAction::LedSet(3, 7, true).to_packet();
+        assert_eq!(packet,
+                   OscPacket::Message(OscMessage {
+                       addr: "/64/grid/led/set".into(),
+                       args: Some(vec![OscType::Int(3), OscType::Int(7), OscType::Int(1)]),
+                   }));
+    }
+
+    #[test]
+    fn led_all_to_packet() {
+        let packet = MonomeAction::LedAll(true).to_packet();
+        assert_eq!(packet,
+                   OscPacket::Message(OscMessage {
+                       addr: "/64/grid/led/all".into(),
+                       args: Some(vec![OscType::Int(1)]),
+                   }));
+    }
+
+    #[test]
+    fn led_intensity_to_packet() {
+        let packet = MonomeAction::LedIntensity(7).to_packet();
+        assert_eq!(packet,
+                   OscPacket::Message(OscMessage {
+                       addr: "/64/grid/led/intensity".into(),
+                       args: Some(vec![OscType::Int(7)]),
+                   }));
+    }
+
+    #[test]
+    fn led_map_to_packet() {
+        let packet = MonomeAction::LedMap(3, 7, &[0, 1, 2, 3, 4, 5, 6, 7]).to_packet();
+        assert_eq!(packet,
+                   OscPacket::Message(OscMessage {
+                       addr: "/64/grid/led/map".into(),
+                       args: Some(vec![OscType::Int(3),
+                                       OscType::Int(7),
+                                       OscType::Int(0),
+                                       OscType::Int(1),
+                                       OscType::Int(2),
+                                       OscType::Int(3),
+                                       OscType::Int(4),
+                                       OscType::Int(5),
+                                       OscType::Int(6),
+                                       OscType::Int(7)]),
+                   }));
+    }
+
+    #[test]
+    fn led_col_to_packet() {
+        let packet = MonomeAction::LedCol(3, 7, 16).to_packet();
+        assert_eq!(packet,
+                   OscPacket::Message(OscMessage {
+                       addr: "/64/grid/led/col".into(),
+                       args: Some(vec![OscType::Int(3), OscType::Int(7), OscType::Int(16)]),
+                   }));
+    }
+
+    #[test]
+    fn led_row_to_packet() {
+        let packet = MonomeAction::LedRow(3, 7, 17).to_packet();
+        assert_eq!(packet,
+                   OscPacket::Message(OscMessage {
+                       addr: "/64/grid/led/row".into(),
+                       args: Some(vec![OscType::Int(3), OscType::Int(7), OscType::Int(17)]),
+                   }));
     }
 }
